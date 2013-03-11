35e8c2f57b0da8e01f058bdd4a1bec99e88ac3df
Wedge <wedgette@gmail.com>
2013-03-11 01:37:41 -0700

    update

--- update.lua
+++ update.lua
@@ -1,4 +1,12 @@
 print("Got this from git!")
+
+while turtle.getFuelLevel < 200
+	if not turtle.refuel(1)
+		sleep(5)
+		print("Out of fuel!")
+	end
+end
+
 for i = 0,3 do
 	if turtle.forward() then
 		turtle.turnLeft()
