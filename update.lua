808c0f5e2ecc0a7ae47190679605a0d1a004a158
Wedge <wedgette@gmail.com>
2013-03-11 01:45:12 -0700

    more looping for reals

--- update.lua
+++ update.lua
@@ -1,12 +1,16 @@
 print("Got this from git!")
 
-while turtle.getFuelLevel() < 200 do
-	if not turtle.refuel(1) then
-		sleep(5)
-		print("Out of fuel!")
+while true do
+
+	while turtle.getFuelLevel() < 200 do
+		if not turtle.refuel(1) then
+			sleep(5)
+			print("Out of fuel!")
+		end
 	end
 	
 	if turtle.forward() then
 		turtle.turnLeft()
 	end
+	
 end
